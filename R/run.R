13f2a6f7205ca19943a2f86b9158088d889b8dc8
Moritz Shore <moritzshore@gmail.com>
2023-03-27 15:37:56 +0200

    Update run.R

--- R/run.R
+++ R/run.R
@@ -34,17 +34,17 @@ run_swap <- function(project_path,
   swap_exe <- work_dir %>% paste(collapse = "/") %>% paste0(.,"/swap.exe")
   swap_file_path <- glue("{project}/rswap/{swap_file}")
 
-  if(file.exists(swap_file_path) == FALSE){
-    stop(glue("SWAP main file not found!\n{project_path}\n{swap_file_path}"))
-  }
-
-
   if(file.exists(swap_exe)==FALSE){
    stop(glue("swap.exe must be located in parent directory of {project}!\n Required Path: {swap_exe}"))
   }
 
   # builds a directory for performing package actions, and returns the path
   rswap_directory <- build_rswap_directory(project_path)
+
+
+  if(file.exists(swap_file_path) == FALSE){
+    stop(glue("SWAP main file not found!\n{project_path}\n{swap_file_path}"))
+  }
 
 
   # reads in the swap parameters and tables
