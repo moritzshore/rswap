d2a21315a5e736ee0060182c03d5babfd2c9d9fd
Moritz Shore <moritzshore@gmail.com>
2023-03-31 18:00:56 +0200

    Update documentation for stat.R

--- R/stat.R
+++ R/stat.R
@@ -1,40 +1,42 @@
-# stat functions
+# Functions relating to statistics
 
-#' Get model performance
+#' Get Model Performance
 #'
-#' returns the performance indicator for the chosen variable(s) and measure(s)
-#' Supported performance indicators are: NSE, PBIAS, RMSE, RSR
-#' Supported variables are those provided in the observed data file
-#' Please note, passing multiple variables at differing depths will not work!
-#' @param project_path **(REQ)** **(string)** path to the project directory
-#' @param archived Flag, if the model run is saved in the rswap_saved archived
-#' @param stat (opt) (string/vector) "NSE" "PBIAS" "RMSE" "RSR" or leave blank for all
-#' @param variable (REQ) (string/vector) variable name (ie. WC, H, TEMP). leave blank for all available.
-#' @param depth (OPT) (numeric/vector) depth of variable value (>0). Must exist
-#' in SWAP output. leave blank for all available
-#' @param verbose (OPT) (boolean) print status to console?
+#' Returns the performance for the chosen variable(s) and performance indicator(s).
+#' Supported performance indicators are: NSE, PBIAS, RMSE, RSR.
+#' Supported variables are those provided in the observed data file.
+#'
+#' Please note, passing multiple variables at differing depths will not work! (yet)
+#'
+#' @param project_path path to project directory (string)
+#' @param archived set to `TRUE` if project is saved in "rswap_saved/" (flag)
+#' @param stat statistical performance indicator (ie. "NSE") (string)
+#' @param variable variable name. leave blank for all. (string)
+#' @param depth depth of variable if it is depth-wise. leave blank for all (numeric)
+#' @param verbose print status? (flag)
+#'
 #' @importFrom dplyr %>% nth
 #' @importFrom tibble tibble
 #' @importFrom glue glue
-#' @returns (dataframe) value(s) of performance indicator(s) for given variable(s) and depth(s)
+#'
+#' @returns dataframe value(s) of performance indicator(s) for given variable(s) and depth(s)
+#'
 #' @export
 get_performance <-
-  function(project_path,
-           archived = FALSE,
-           stat = NULL,
-           variable = NULL,
-           depth = NULL,
-           verbose = F) {
+  function(project_path, archived = FALSE,
+           stat = NULL, variable = NULL,
+           depth = NULL, verbose = F) {
+
+    # TODO rename to get_swap_performance()
 
     observed_file_path <- glue("{project_path}/rswap_observed_data.xlsx")
 
-      if(variable %>% is.null()){
-        obs <- load_observed(project_path)
-        variable = obs$observed_variables
-      }
+    if (variable %>% is.null()) {
+      obs <- load_observed(project_path)
+      variable = obs$observed_variables
+    }
 
-    rlist <-
-      match_mod_obs(
+    rlist <- match_mod_obs(
         project_path = project_path,
         variable = variable,
         depth = depth,
@@ -42,24 +44,24 @@ get_performance <-
         archived = archived
       )
 
+    # unpack the list
     modelled_data_filtered = rlist$mod
     observed_data_filtered = rlist$obs
 
     obs_col_length = length(observed_data_filtered)
 
-
-    # from 2 because skip date col
     return_df <- data.frame()
-    for (i in c(2:obs_col_length)) {
+    for (i in c(2:obs_col_length)) {  # from 2 because skip date col
+
       obs_join = data.frame(DATE=observed_data_filtered$DATE, obs = observed_data_filtered[i])
       mod_join = data.frame(DATE=modelled_data_filtered$DATE, obs = modelled_data_filtered[i])
       name = obs_join %>% colnames() %>% nth(2)
-
 
       joined_df <- data.frame(DATE = observed_data_filtered$DATE)
       joined_df <- left_join(joined_df, obs_join, by = "DATE")
       joined_df <- left_join(joined_df, mod_join, by = "DATE")
       colnames(joined_df)<-c("DATE", "obs", "mod")
+
       nse_val = NSE(obs = joined_df$obs, mod = joined_df$mod)
       pbias_val = PBIAS(obs = joined_df$obs, mod = joined_df$mod)
       rmse_val = RMSE(obs = joined_df$obs, mod = joined_df$mod)
@@ -69,7 +71,7 @@ get_performance <-
       return_df = rbind(return_df, row)
     }
 
-    # filter by stat if givne
+    # filter by stat if given
     if(stat %>% is.null()==FALSE){
       return_df<-return_df %>% select(var, all_of(stat))
     }
@@ -79,13 +81,17 @@ get_performance <-
 
 #' NSE
 #'
-#' Returns NSE value for a paired observed and modeled vector.
-#' #TODO: change this function to accept a named vector, or dataframe.
-#' @param obs numeric vector of observed values **REQ**
-#' @param mod numeric vector of modeled values **REQ**
-#' @return numeric NSE value rounded to 2 decimals
+#' Calculates the NSE value for a paired observed and modeled vector.
+#'
+#' @param obs numeric vector of observed values
+#' @param mod numeric vector of modeled values
+#'
+#' @returns numeric NSE value rounded to 2 decimals
+#'
 #' @export
+#'
 NSE <- function(obs, mod) {
+  #TODO: change this function to accept a named vector, or dataframe.
   return((1 - (sum((obs - mod) ^ 2, na.rm = T
   ) / sum((obs - mean(obs, na.rm = T)) ^ 2, na.rm = T
   ))) %>% round(x = ., digits = 2))
@@ -94,36 +100,45 @@ NSE <- function(obs, mod) {
 #' PBIAS
 #'
 #' Returns PBIAS value for a paired observed and modeled vector.
-#' #TODO: change this function to accept a named vector, or dataframe.
-#' @param obs numeric vector of observed values **REQ**
-#' @param mod numeric vector of modeled values **REQ**
-#' @return numeric PBIAS value rounded to 2 decimal
+#'
+#' @param obs numeric vector of observed values
+#' @param mod numeric vector of modeled values
+#'
+#' @returns numeric PBIAS value rounded to 2 decimal
+#'
 #' @export
 
 PBIAS <-function(obs, mod) {
+  #TODO: change this function to accept a named vector, or dataframe.
   return(((sum(obs - mod, na.rm = T) * 100) / sum(obs, na.rm = T)) %>% round(x = ., digits = 2))
 }
 
 #' RMSE
 #'
 #' Returns RMSE value for a paired observed and modeled vector.
-#' #TODO: change this function to accept a named vector, or dataframe.
-#' @param obs numeric vector of observed values **REQ**
-#' @param mod numeric vector of modeled values **REQ**
-#' @return numeric RMSE value rounded to 2 digits
+#'
+#' @param obs numeric vector of observed values
+#' @param mod numeric vector of modeled values
+#'
+#' @returns numeric RMSE value rounded to 2 digits
+#'
 #' @export
 RMSE <- function(obs, mod) {
+  #TODO: change this function to accept a named vector, or dataframe.
   return(sqrt(sum((obs - mod) ^ 2, na.rm = T) / length(obs)) %>% round(x = ., digits = 2))
 }
 
 #' RSR
 #'
 #' Returns RSR value for a paired observed and modeled vector.
-#' #TODO: change this function to accept a named vector, or dataframe.
-#' @param obs numeric vector of observed values **REQ**
-#' @param mod numeric vector of modeled values **REQ**
-#' @return numeric RSR value rounded to 2 digits
+#'
+#' @param obs numeric vector of observed values
+#' @param mod numeric vector of modeled values
+#'
+#' @returns numeric RSR value rounded to 2 digits
+#'
 #' @export
 RSR <- function(obs, mod) {
+  # TODO: change this function to accept a named vector, or dataframe.
   return(RMSE(obs, mod) / sd(obs, na.rm = T) %>% round(x = ., digits = 2))
 }
