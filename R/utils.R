4aa6dca0d6ae930ba6263253aed26eed523b2b69
Moritz Shore <moritzshore@gmail.com>
2023-04-05 15:24:15 -0400

    add install_SWAPtools()

--- R/utils.R
+++ R/utils.R
@@ -32,6 +32,29 @@ get_swap_units <- function(variable){
   }
 
   if(unit %>% is.na()){warning("variable not listed")}
+#' Install SWAPtools
+#'
+#' Installs SWAPtools from waterwijzerlandbouw.wur.nl/repo
+#'
+#' @returns returns TRUE if package is already installed, or if installation
+#' was successful. returns FALSE if installation failed.
+#'
+install_SWAPtools <- function() {
+  if ("SWAPtools" %in% installed.packages()) {
+    return(TRUE)
+  } else{
+    cat("\n SWAPtools is required for this functionality. Installing...\n")
 
   return(unit)
+    install.packages(pkg = "SWAPtools",
+                     dependencies = TRUE,
+                     repos = "https://waterwijzerlandbouw.wur.nl/repo",)
+    if ("SWAPtools" %in% installed.packages()) {
+      cat("\nSWAPtools installed succesfully\n")
+      return(TRUE)
+    } else{
+      cat("\n SWAPtools install unsuccessfull! ")
+      return(FALSE)
+    }
+  }
 }
