4ebcd2f37be8d3ac7b960606c0afb38947f74539
Moritz Shore <moritzshore@gmail.com>
2023-04-25 10:53:53 -0400

    Update `rswap_plot_performance()`
    Updated to support `hydroGOF` stat functions.

--- R/plot_performance.R
+++ R/plot_performance.R
@@ -2,17 +2,26 @@
 #'
 #' This function calculates the statistical performance of all the saved model
 #' runs in the /rswap_saved/ folder, as well as the last performed run. It does
-#' this per `variable` and for any/all given `depths`, for the passed statistical
-#' performance indicator `stat` (Currently supported are "NSE", "RMSE", "PBIAS", "RSR")
+#' this per `variable` and for any/all given `depths`, for the passed
+#' statistical performance indicator `stat` from package
+#' [hydroGOF](https://cran.r-project.org/web/packages/hydroGOF/hydroGOF.pdf)
+#'
+#' The performance indicator will be the result of a function passed as a string
+#' in the `stat` parameter as called from the `hydroGOF` package.
+#'
+#' The following are supported for `stat`:
+#'
+#' *br2 cp d KGE mae md me mNSE mse nrmse NSE pbias rd rmse rNSE rPearson rSD rsr ssq VE*
+#'
+#' Please note, passing multiple variables at differing depths will not work!
+#' (yet)
 #'
 #' @param project_path path to project directory (string)
 #' @param variable variable to show (string)
 #' @param depth depth(s) to show. Leave blank for all (numeric/vector)
-#' @param graph either "default" "sorted" or "ggplot" leave blank for default (string)
-#' @param stat statistical indicator function ("NSE", "RMSE", "PBIAS", "RSR") (string)
+#' @param stat a statistical indicator function from package `hydroGOF` (string)
 #' @param verbose print status? (flag)
 #'
-#' @importFrom ggplot2 ggplot theme scale_color_manual geom_col aes geom_line geom_point labs ylab xlab ggtitle scale_fill_manual
 #' @importFrom dplyr %>% pull select all_of filter
 #' @importFrom stringr str_split
 #' @importFrom glue glue
@@ -23,15 +32,7 @@
 #' @export
 #'
 rswap_plot_performance <-
-  function(project_path, variable, depth =  NULL, graph = "default",stat = "NSE",
-           verbose = F) {
-
-  graph = graph %>% tolower()
-
-  if (graph %in% c("ggplot", "default", "sorted") == FALSE) {
-      graph = "default"
-      warning("graph type not recognized! supported are:\n 'default', 'sorted', ggplot'")
-    }
+  function(project_path, variable, stat, depth =  NULL, verbose = FALSE) {
 
   # extract the run name from the data package
   current_run <- project_path %>% stringr::str_split("./") %>% unlist() %>% utils::tail(1)
@@ -54,8 +55,6 @@ rswap_plot_performance <-
 
     # extract the run name from the data package
     run_name <- file %>% stringr::str_split("./") %>% unlist() %>% utils::tail(1)
-
-    if(length(stat)>1){stop("rswap ERROR: please pass only one stat value")}
 
       performance = get_swap_performance(
         project_path = file,
@@ -83,7 +82,7 @@ rswap_plot_performance <-
   )
 
   last_run$run = "last_run"
-  last_run$mean = last_run %>% dplyr::select(dplyr::all_of(stat)) %>% dplyr::pull() %>% mean()
+  last_run$mean = last_run %>% dplyr::select("value") %>% dplyr::pull() %>% mean()
 
   stat_df <- rbind(stat_df, last_run)
 
@@ -92,10 +91,10 @@ rswap_plot_performance <-
 
     fig1 <- plotly::plot_ly()
 
-    # dont need columns if not depthwise
+    # don't need columns if not depth wise
     if(depth %>% is.null() == FALSE){
       num_colors = stat_df$variable %>% unique() %>% length()
-      # columns of NSE value per depth
+      # columns of stat value per depth
       fig1 <-
         fig1 %>% plotly::add_trace(
           data = stat_df,
